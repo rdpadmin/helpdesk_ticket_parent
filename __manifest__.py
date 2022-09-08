b6e613b6a2f240e1836746a377d646a136cca1e6
RDP <admin@rdp.in>
2022-09-08 13:32:15 +0530

    manifest added

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,11 @@
-# Copyright (C) 2019 - TODAY, Open Source Integrators
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
 {
-    'name': 'Helpdesk Ticket Parent',
+    'name': 'Helpdesk Parent and Child Ticket',
     'summary': 'Add Parent/Child relationship to Helpdesk Ticket',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0',
     'category': 'Helpdesk',
-    'author': "Open Source Integrators",
-    'website': 'https://github.com/ursais/osi-addons',
+    'author': "RDP",
+    'website': 'https://www.rdp.in',
     'depends': [
         'helpdesk_timesheet'
     ],
@@ -16,9 +15,5 @@
     'installable': True,
     'license': 'LGPL-3',
     'development_status': 'Beta',
-    'maintainers': [
-        'wolfhall',
-        'max3903',
-        'osi-scampbell'
-    ],
+    
 }
